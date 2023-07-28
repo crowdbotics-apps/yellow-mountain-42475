07655efd2a1dc0fb7d78d3c9ed28334b05cbea6e
Crowdbotics <team@crowdbotics.com>
2023-07-28 01:30:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-mountain-42475",
+  url: "https://yellow-mountain-42475.botics.co",
+  api: "https://yellow-mountain-42475.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
